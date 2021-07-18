16d6fab2bd9753af10e26ff7b515c881709283ee
AlexisData <alexis.dana91@gmail.com>
2021-07-18 22:29:26 +0200

    staged config

--- config.py
+++ config.py
@@ -4,8 +4,27 @@ HOST = "localhost"
 USER = "root"
 PASSWD = "garyrouch"
 
+# Proxy Configuration
+
+PROXY = 'https://51.210.105.57:24000'
+
 # Constant values
 
 FIRST_ELEMENT = 0
+BASE_JOB_RESULTS_URL = "https://www.indeed.com/jobs?q="
 BASE_JOB_POST_URL = "https://www.indeed.com/viewjob?jk="
 NUMBER_OF_LINK = 15
+
+# Argparse helpers
+
+KEYWORD_HELPER = "Keyword of the job you are looking for"
+PLACE_HELPER = "Where do you want to work"
+NUMBER_PAGE_HELPER = "Number of pages to scrape"
+
+# Indeed tags
+
+COMPANY_NAME_TAG = "icl-u-lg-mr--sm icl-u-xs-mr--xs"
+COMPANY_RATING_TAG = "jobsearch-InlineCompanyRating icl-u-xs-mt--xs jobsearch-DesktopStickyContainer-companyrating"
+COMPANY_FOOTER = "jobsearch-JobMetadataFooter"
+JOB_DESCRIPTION_SECTION = "jobsearch-JobDescriptionSection-sectionItem"
+CANDIDATE_LINK_LOCATOR = "icl-Button icl-Button--primary icl-Button--md icl-Button--block"
